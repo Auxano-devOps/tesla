5c7282a81064043b058180e8a626c01d425a4845
Ray <etiendem.blessedray@gmail.com>
2022-08-31 19:32:43 +0000

    deployment ready

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 print('Ray')
 # echo Ray
+print('We are winning')
+print('Rayne')
+print('Jesu is Lord in our Relationship')
+#echo "It pays to serve the Lord Jesus Christ"
+print('Pastor Dan')
+print('We are making Jesus center in the USA')
